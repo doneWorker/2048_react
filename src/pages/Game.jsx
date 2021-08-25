110c4f44b44e778a6603962d2e062a7099365e13
Kirill Anikin <anikinkirill22@gmail.com>
2021-08-26 00:34:44 +0500

    feat: styles changes, generating tiles

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,12 +3,14 @@ import styles from "./Game.module.css";
 
 // TODO: change board dynamic
 const BOARD_SIZE = 16;
-const TILE_SIZE = 200;
-const TILE_OFFSET = 10;
+const TILE_SIZE = 180;
+const TILE_OFFSET = 15;
 
 // UTILS
-function getNextNumber(score) {
-  return 2;
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function getTilePosition(
@@ -17,17 +19,36 @@ function getTilePosition(
   tileSize = TILE_SIZE,
   margin = TILE_OFFSET
 ) {
-  const w = boardSize / 4;
-  const m = (index % w) * margin;
-  const yPos = Math.ceil((index + 1) / w) - 1;
+  const w = boardSize / 4,
+    r = index % w,
+    c = Math.floor(index / w),
+    mx = margin * (r + 1),
+    my = margin * (c + 1);
 
   return {
-    x: m + (index % w) * tileSize + m,
-    y: yPos * tileSize,
+    x: mx + r * tileSize,
+    y: my + c * tileSize,
   };
 }
 
-function Tile({ index }) {
+function getNextTile(matrix) {
+  const getFreeCells = (matrix) => {
+    const freeCells = [];
+    matrix.forEach((_, i) => matrix[i] === 0 && freeCells.push(i));
+    return freeCells;
+  };
+
+  const possibleNumbers = [2, 4],
+    randomNumber =
+      possibleNumbers[getRandomIntInclusive(0, possibleNumbers.length - 1)],
+    freeCells = getFreeCells(matrix),
+    position = freeCells[getRandomIntInclusive(0, freeCells.length - 1)];
+
+  return [randomNumber, position];
+}
+
+// COMPONENTS
+function Tile({ index, value }) {
   const [pos, setPos] = useState(getTilePosition(index));
 
   useEffect(() => {
@@ -39,7 +60,7 @@ function Tile({ index }) {
       className={styles.tile}
       style={{ left: `${pos.x}px`, top: `${pos.y}px` }}
     >
-      1
+      {value}
     </div>
   );
 }
@@ -47,21 +68,33 @@ function Tile({ index }) {
 function Board({ children }) {
   return (
     <div className={styles.board}>
-      <div>
+      <div className={styles.boardBackground}>
         {new Array(BOARD_SIZE).fill(0).map((el, idx) => {
           return <div className={styles.gridCell} key={idx} />;
         })}
       </div>
-      {/* {children} */}
+      {children}
     </div>
   );
 }
 
 export default function Game() {
+  const [tiles, setTiles] = useState(new Array(BOARD_SIZE).fill(0));
+
+  useEffect(() => {
+    setTiles((prevState) => {
+      const [num, pos] = getNextTile(tiles);
+      let newState = [...prevState];
+      newState[pos] = num;
+      return newState;
+    });
+  }, []);
+
+  console.log(tiles);
   return (
     <Board size={BOARD_SIZE}>
-      {new Array(BOARD_SIZE).fill(0).map((el, idx) => {
-        return <Tile index={idx} key={idx} />;
+      {tiles.map((el, idx) => {
+        return el !== 0 && <Tile index={idx} key={idx} value={el} />;
       })}
     </Board>
   );
